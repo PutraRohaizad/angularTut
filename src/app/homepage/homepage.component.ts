b0a07f270c76216419e4d292a7133bf4b5dc1f45
Putra <putrarohayzad@gmail.com>
2019-05-07 12:52:39 +0800

    Home title from static to dynamic

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -6,6 +6,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomepageComponent implements OnInit {
 
+  homeTitle : string = "Homepage";
+
   constructor() { }
 
   ngOnInit() {
